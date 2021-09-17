8d87e1f32c2ef9e4389aee5e8d054acd761ca92e
Ryan Cary <rcary@macbook-pro.myfiosgateway.com>
2021-09-17 10:58:34 -0400

    add https port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 import express from "express";
 import routes from "./routes";
+import https from 'https'
+import fs from 'fs';
+import path from 'path';
 
+const key = fs.readFileSync(path.resolve(__dirname + '../../../certs/localhost+2-key.pem'));
+const cert = fs.readFileSync(path.resolve(__dirname + '../../../certs/localhost+2.pem'));
+const options = {
+  key, 
+  cert,
+  requestCert: false,
+  rejectUnauthorized: false
+}
 const app = express();
 
 app.use(express.json());
@@ -12,6 +23,10 @@ app.get("/", (req, res) => {
   res.send(`I'm Alive!`);
 });
 
-app.listen(3000, () => {
+const server = https.createServer(options, app);
+
+app.listen(3001, () => console.log("Listening on port 3001"));
+
+server.listen(3000, () => {
   console.log("Listening on port 3000 🚀");
 });
